604ebe2dd5d768e09441849119defc5ea5674cc0
sebastian <94870277+EmaHermankova@users.noreply.github.com>
2022-11-25 08:28:09 +0100

    Add files via upload

---
+++ Datum.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Datum implements Comparable<Datum>{
+
+    int den;
+    int mesic;
+    int rok;
+
+    public Datum(int den, int mesic, int rok) {
+
+        this.den = den;
+        this.mesic = mesic;
+        this.rok = rok;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Datum{" +
+                "den=" + den +
+                ", mesic=" + mesic +
+                ", rok=" + rok +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Datum o) {
+
+        if(rok<o.rok){
+            return -1;
+        }
+        if(rok>o.rok){
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Datum[] arr = {
+                new Datum(25,11,2022),
+                new Datum(31,12,2005),
+                new Datum(14,2,2022),
+                new Datum(18,3,2005),
+                new Datum(6,5,1977)
+        };
+
+        Arrays.sort(arr);
+        for (Datum d:arr){
+            System.out.println(d);
+        }
+    }
+
+}
